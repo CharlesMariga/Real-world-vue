86b3f16a2892bcb0083bc8e63fad977033fa63d5
charlesmariga <charlesmariga37@gmail.com>
2022-10-15 20:49:44 +0300

    programmatic navigation

--- src/views/event/EventRegister.vue
+++ src/views/event/EventRegister.vue
@@ -1,10 +1,18 @@
 <template>
   <p>Register for event here</p>
+  <button @click="register">Register Me</button>
 </template>
 
 <script>
 export default {
   name: "EventDetails",
   props: ["event"],
+  methods: {
+    register() {
+      this.$router.push({
+        name: "event-details",
+      });
+    },
+  },
 };
 </script>
